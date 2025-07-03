65f633ed57906b7d0d92e5a0e27a736b6a80adaa
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-03 15:04:14 +0500

    created_at added

--- config/bot/admin.py
+++ config/bot/admin.py
@@ -92,6 +92,9 @@ class ExcelUploadAdmin(admin.ModelAdmin):
 
                         # Handle created date - matching command logic
                         date = str(row.get("Yaratilgan vaqti")).strip() if pd.notna(row.get("Yaratilgan vaqti")) else None
+
+                        print(date)
+
                         created_date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S") if date else first_payed_date
 
                         status = 'COMPLETED' if str(row['Buyurtma statusi']).strip().upper() == 'COMPLETED' else 'ACTIVE'
